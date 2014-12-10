b82478c1bc5b84aca3e0ce9d2b3ddec69c9ad2bf
Sebastien Saunier <seb@saunier.me>
2014-12-10 18:41:25 +0100

    Seed movies

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,3 @@
 class Movie < ActiveRecord::Base
+  validates_presence_of :title, :imdb_id, :url, :year, :director
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  root to: 'movies/index'
+  root to: 'movies#index'
 end
