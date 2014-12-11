b7c0eba8d52f854976baede99a3b3c11ec3734e0
Sebastien Saunier <seb@saunier.me>
2014-12-11 10:33:55 +0100

    implement algolia

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,19 @@
 class Movie < ActiveRecord::Base
   # validates_presence_of :title, :imdb_id, :url, :year, :director
+
+  has_many :actors
+
+  include AlgoliaSearch
+
+  algoliasearch index_name: "Movie#{ENV['ALGOLIA_SUFFIX']}" do
+    # associated index settings can be configured from here
+
+    attributesToIndex ['title', 'director', 'year']
+
+    # attributesToFaceting [ 'year' ]
+
+    # add_attribute :actors do
+    #   actors.map &:name
+    # end
+  end
 end
